3c159f18200b3b9f23e0889d41aceb6139d411f2
isabele picksius <106542913+barbiebutterfly@users.noreply.github.com>
2024-10-04 08:41:28 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,14 +1,12 @@
-
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body);
-    return bodyStyles.getPropertyValue(variavel);
-};
+    const bodyStyles = getComputedStyle(document.body)
+    return bodyStyles.getPropertyValue(variavel)
+}
 
-
-const createTickConfig = () => ({
+const tickConfig = {
     family: getCSS('--font'),
     size: 16,
     color: getCSS('--primary-color')
-});
+}
 
-export { getCSS, createTickConfig };
+export {getCSS, tickConfig}
